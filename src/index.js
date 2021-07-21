6d5eea42dadec10667bac908ae97edf5a810f5d8
muhenge <mugunga5@gmail.com>
2021-07-21 21:03:34 +0200

    Sort the list based on the index and add onload to render DOM on load

--- src/index.js
+++ src/index.js
@@ -25,9 +25,26 @@ const todoLists = [
   },
 ];
 
-todoLists.forEach(todo => {
-  const li = document.createElement('li');
-  const ul = document.getElementById('todo-lists');
-  li.innerText = todo.description;
-  ul.appendChild(li);
-});
+const compare = (x, y) => {
+  const indexX = x.index;
+  const indexY = y.index;
+
+  let comp = 0;
+  if (indexX > indexY) {
+    comp = 1;
+  } else if (indexX < indexY) {
+    comp = -1;
+  }
+  return comp;
+};
+
+const sorted = todoLists.sort(compare);
+
+window.onload = () => {
+  sorted.forEach(todo => {
+    const li = document.createElement('li');
+    const ul = document.getElementById('todo-lists');
+    li.innerText = todo.description;
+    ul.appendChild(li);
+  });
+};
